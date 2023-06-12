4f31d45a1d4f28764e4a708532dad11fe3bb60bd
Kyle Crawford <kcraw217@gmail.com>
2023-06-12 16:02:14 -0700

    add CRUD
    co-authored-by: Tom McDowell <tmcdowellddhs@yahoo.com>
    co-authored-by: Eusebie Siebenberg <siebenee@gmail.com>"

--- MouseMingleClient/Controllers/InterestsController.cs
+++ MouseMingleClient/Controllers/InterestsController.cs
@@ -8,7 +8,45 @@ public class InterestsController : Controller
   public IActionResult Index()
   {
     List<Interest> interests = Interest.GetAll();
-    
+
     return View(interests);
+  }
+
+  public IActionResult Details(int id)
+  {
+    var interest = Interest.GetDetails(id);
+
+    return View(interest);
+  }
+
+  public ActionResult Create()
+  {
+    return View();
+  }
+
+  [HttpPost]
+  public ActionResult Create(Interest interest)
+  {
+    Interest.Post(interest);
+    return RedirectToAction("Index");
+  }
+
+  public ActionResult Edit(int id)
+  {
+    Interest interest = Interest.GetDetails(id);
+    return View(interest);
+  }
+
+  public ActionResult Delete(int id)
+  {
+    Interest interest = Interest.GetDetails(id);
+    return View(interest);
+  }
+
+  [HttpPost, ActionName("Delete")]
+  public ActionResult DeleteConfirmed(int id)
+  {
+    Interest.Delete(id);
+    return RedirectToAction("Index");
   }
 }
