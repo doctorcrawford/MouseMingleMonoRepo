f9d4bd24c4fa9fa91a8d620ff67507644b475e18
Emma Gerigscott <elgerig@gmail.com>
2023-06-12 15:45:39 -0700

    update get methods to include list of interests for each rodent
    co-authored-by: Sarah Reimann <reimannsarah45@gmail.com>

--- MouseMingleApi/Controllers/RodentsController.cs
+++ MouseMingleApi/Controllers/RodentsController.cs
@@ -1,6 +1,7 @@
 using MouseMingleApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 
 
 namespace MouseMingleApi.Controllers;
@@ -8,6 +9,7 @@ namespace MouseMingleApi.Controllers;
 [ApiController]
 [Route("api/v{version:ApiVersion}/[controller]")]
 [ApiVersion("1.0")]
+[ApiVersion("1.1")]
 public class RodentsController : Controller
 {
   private readonly MouseMingleApiContext _db;
@@ -20,13 +22,17 @@ public class RodentsController : Controller
   [HttpGet]
   public async Task<ActionResult<IEnumerable<Rodent>>> GetRodents()
   {
-    return await _db.Rodents.ToListAsync();
+    return await _db.Rodents
+                    .Include(rodent => rodent.RodentInterests)
+                    .ToListAsync();
   }
 
   [HttpGet("{id}")]
   public async Task<ActionResult<Rodent>> GetRodent(int id)
   {
-    Rodent thisRodent = await _db.Rodents.FindAsync(id);
+    Rodent thisRodent = await _db.Rodents
+                                        .Include(r => r.RodentInterests)
+                                        .FirstOrDefaultAsync(r =>r.RodentId == id);
     if (thisRodent == null)
     {
       return NotFound();
