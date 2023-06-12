d0b61a928a6ee3d99fec51eb5b773fda4a4317d0
Kyle Crawford <kcraw217@gmail.com>
2023-06-12 16:03:08 -0700

    add properties and static APIHelper methods
    co-authored-by: Tom McDowell <tmcdowellddhs@yahoo.com>
    co-authored-by: Eusebie Siebenberg <siebenee@gmail.com>"

---
+++ MouseMingleClient/Models/Interest.cs
@@ -0,0 +1,50 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MouseMingleClient.Models;
+
+public class Interest
+{
+  public int InterestId { get; set; }
+  public string Description { get; set; }
+  public List<RodentInterest> JoinEntities { get; }
+
+  public static List<Interest> GetAll()
+  {
+    var apiCallTask = ApiHelper.GetAllInterestsAsync();
+    var result = apiCallTask.Result;
+
+    var jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+    List<Interest> interestList = JsonConvert.DeserializeObject<List<Interest>>(jsonResponse.ToString());
+
+    return interestList;
+  }
+
+  public static Interest GetDetails(int id)
+  {
+    var apiCallTask = ApiHelper.Get(id);
+    var result = apiCallTask.Result;
+
+    var jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+    Interest interest = JsonConvert.DeserializeObject<Interest>(jsonResponse.ToString());
+
+    return interest;
+  }
+
+  public static void Post (Interest interest)
+  {
+    string jsonInterest = JsonConvert.SerializeObject(interest);
+    ApiHelper.PostRodentAsync(jsonInterest);
+  }
+
+    public static void Put(Interest interest)
+  {
+    string jsonInterest = JsonConvert.SerializeObject(interest);
+    ApiHelper.Put(interest.InterestId, jsonInterest);
+  }
+
+  public static void Delete(int id)
+  {
+    ApiHelper.Delete(id);
+  }
+}
