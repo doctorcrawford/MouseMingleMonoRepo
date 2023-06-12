fd09aed445540b59bfddbf2220b01f25d4cadbbb
Kyle Crawford <kcraw217@gmail.com>
2023-06-12 15:01:50 -0700

    add register and login methods
    co-authored-by: Tom McDowell <tmcdowellddhs@yahoo.com>
    co-authored-by: Eusebie Siebenberg <siebenee@gmail.com>"

---
+++ MouseMingleClient/Models/ApplicationUser.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Identity;
+using MouseMingleClient.ViewModels;
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace MouseMingleClient.Models;
+
+public class ApplicationUser : IdentityUser
+{
+
+  public static void Register(RegisterViewModel newUser)
+  {
+    var jsonNewUser = JsonConvert.SerializeObject(newUser);
+    ApiHelper.RegisterUserAsync(jsonNewUser);
+  }
+
+  public static void Login(LoginViewModel user)
+  {
+    var jsonUser = JsonConvert.SerializeObject(user);
+    ApiHelper.LoginUserAsync(jsonUser);
+  }
+}
