bff41be2502d1bb1c7fdf4f6bea70dd3d13f0b18
Emma Gerigscott <elgerig@gmail.com>
2023-06-12 11:18:43 -0700

    add interest and rodentinterest model, update dbcontext
    co-authored-by: Sarah Reimann <reimannsarah45@gmail.com>

--- MouseMingleApi/Models/MouseMingleContext.cs
+++ MouseMingleApi/Models/MouseMingleContext.cs
@@ -5,5 +5,19 @@ namespace MouseMingleApi.Models;
 
 public class MouseMingleApiContext : DbContext
 {
-  public DbSet<Rat> Rats { get; set; }
+  public DbSet<Rodent> Rodents { get; set; }
+  public DbSet<Interest> Interests { get; set; }
+  public DbSet<RodentInterest> RodentInterests { get; set; }
+
+  public MouseMingleApiContext(DbContextOptions options) : base(options) {}
+
+  protected override void OnModelCreating(ModelBuilder builder)
+  {
+    builder.Entity<Rodent>()
+      .HasData(
+        new Rodent { RodentId = 1, Species = "Rat", Name = "Scorpo Fangoria", Age = 43, Location = "Fishermans Warf No.3", ProfilePic = "./imagePath.img"},
+        new Rodent { RodentId = 2, Species = "Rat", Name = "Cadder Jenks", Age = 33, Location = "In a van down by the river", ProfilePic ="./imagePath.img"},
+        new Rodent { RodentId = 3, Species = "Mouse", Name = "Flippy Twitch", Age = 3, Location = "hole", ProfilePic = "./iumagePath.img"}
+      );
+  }
 }
---
+++ MouseMingleApi/Models/RodentInterest.cs
@@ -0,0 +1,8 @@
+namespace MouseMingleApi.Models;
+
+public class RodentInterest
+{
+  public int RodentInterestId { get; set; }
+  public int RodentId { get; set; }
+  public int InterestId { get; set; }
+}
