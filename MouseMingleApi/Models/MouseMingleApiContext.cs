cabeb1a75477319a1c7eb39f49f45d5899c0cb36
Emma Gerigscott <elgerig@gmail.com>
2023-06-12 11:42:35 -0700

    seed interest data
    co-authored-by: Sarah Reimann <reimannsarah45@gmail.com>

--- MouseMingleApi/Models/MouseMingleApiContext.cs
+++ MouseMingleApi/Models/MouseMingleApiContext.cs
@@ -19,5 +19,18 @@ public class MouseMingleApiContext : DbContext
         new Rodent { RodentId = 2, Species = "Rat", Name = "Cadder Jenks", Age = 33, Location = "In a van down by the river", ProfilePic ="./imagePath.img"},
         new Rodent { RodentId = 3, Species = "Mouse", Name = "Flippy Twitch", Age = 3, Location = "hole", ProfilePic = "./iumagePath.img"}
       );
+    builder.Entity<Interest>()
+      .HasData(
+        new Interest { InterestId = 1, Description = "Sipping water"},
+        new Interest { InterestId = 2, Description = "Styrofoam"},
+        new Interest { InterestId = 3, Description = "Old peanuts"},
+        new Interest { InterestId = 4, Description = "Long walks under the boardwalk"},
+        new Interest { InterestId = 5, Description = "Wet old towels"},
+        new Interest { InterestId = 6, Description = "Cheese scraps"},
+        new Interest { InterestId = 7, Description = "Meat sticks"},
+        new Interest { InterestId = 8, Description = "Shards"},
+        new Interest { InterestId = 9, Description = "Clam shells"},
+        new Interest { InterestId = 10, Description = "Bye"}
+      );
   }
 }
