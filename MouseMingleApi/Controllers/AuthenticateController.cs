c87348fb50ca08b77d220a9741cd16ca5ae36047
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-13 09:19:00 -0700

    initial
    Co-Authored-By: Kyle Crawford <kcraw217@gmail.com>"

---
+++ MouseMingleApi/Controllers/AuthenticateController.cs
@@ -0,0 +1,13 @@
+using MouseMingleApi.DTO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MouseMingleApi.Controllers;
+
+// [ApiController]
+// [Route("api/v{version:ApiVersion}/[controller]")]
+// [ApiVersion("1.1")]
